21a88074737ae813ce5534c912465f56d58badcf
ShulkeredTV <143276808+ShulkeredTV@users.noreply.github.com>
2023-08-26 09:48:08 -0700

    Add files via upload

---
+++ forgotmdp.js
@@ -0,0 +1,5 @@
+var BtnConfirm = document.getElementById("button-confirm")
+var error = document.getElementById("error")
+var Input1 = document.getElementById("input")
+
+BtnConfirm.onclick = () => error.innerHTML = "Aucun compte n'est lié à cette adresse email."
---
+++ register.js
@@ -0,0 +1,5 @@
+var BtnConfirm = document.getElementById("button-confirm")
+var error = document.getElementById("error")
+var Input1 = document.getElementById("input")
+
+BtnConfirm.onclick = () => error.innerHTML = "La création du compte n'a pas pu se réaliser."
