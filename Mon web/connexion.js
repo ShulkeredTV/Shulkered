67d5566fca4853820e556720996f1093f2f8e47b
ShulkeredTV <143276808+ShulkeredTV@users.noreply.github.com>
2023-08-26 09:41:56 -0700

    Delete Mon web directory

--- Mon web/connexion.js
+++
@@ -1,7 +0,0 @@
-/* Déclaration des variables */
-
-var BtnConfirm = document.getElementById("button-confirm");
-var error = document.getElementById("error")
-
-BtnConfirm.onclick = () => error.innerHTML = "L'adresse email où le mot de passe est incorrect"
-
